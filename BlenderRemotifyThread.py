11266564a4e52f4e21c1d14209fbcdf821e3e8a0
biteworks <mail@tobiaswilhelm.com>
2021-04-03 14:26:00 +0200

    Switched back to UDP

--- BlenderRemotifyThread.py
+++ BlenderRemotifyThread.py
@@ -25,16 +25,13 @@ class BlenderRemotifyThread(threading.Thread):
 
     def run(self):
         # Setup the network socket.
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.settimeout(1)
         sock.bind(('127.0.0.1', 3000))
-        sock.listen(1)
 
         # Receive new data from the client.
         while self.running:
             try:
-                connection, client_address = sock.accept()
-                print('connection from', client_address)
                 self.data = struct.unpack('7f', sock.recv(7*4))
             except socket.timeout:
                 pass
