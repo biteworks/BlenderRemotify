036c9e4bbf9298dd019ea04b09d5ef9f6349f578
biteworks <mail@tobiaswilhelm.com>
2021-04-06 17:55:41 +0200

    Updated socket server to by accessable from every IP

--- BlenderRemotifyThread.py
+++ BlenderRemotifyThread.py
@@ -27,7 +27,7 @@ class BlenderRemotifyThread(threading.Thread):
         # Setup the network socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.settimeout(1)
-        sock.bind(('127.0.0.1', 3000))
+        sock.bind(('', 3000))
 
         # Receive new data from the client
         while self.running:
